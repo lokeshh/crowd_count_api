4228434b714269606e01f3f1b78f1439c7591e86
lokeshh <lokeshhsharma@gmail.com>
2017-11-04 12:39:29 +0000

    add shop model

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  
+  root 'shops#index'
+  get 'shop/:id' => 'shops#show_count'
 end
